c6f77860bda8b1cf239584af886c2ce7eda98a21
JamesJeffryes <jamesgjeffryes@gmail.com>
2019-01-15 18:01:56 -0600

    make spec

--- MSAUtils.spec
+++ MSAUtils.spec
@@ -3,14 +3,61 @@ A KBase module: MSAUtils
 */
 
 module MSAUtils {
+  /* A boolean - 0 for false, 1 for true.
+    @range (0, 1)
+  */
+  typedef int boolean;
+
+  /* An X/Y/Z style reference
+    @id ws
+  */
+  typedef string obj_ref;
+
     typedef structure {
-        string report_name;
-        string report_ref;
-    } ReportResults;
+      string input_shock_id;
+      string input_file_path;
+      string input_staging_file_path;
+      string msa_name;
+      string description;
+      string workspace_name;
+  } ImportMSAParams;
 
-    /*
-        This example function accepts any number of parameters and returns results in a KBaseReport
-    */
-    funcdef run_MSAUtils(mapping<string,UnspecifiedObject> params) returns (ReportResults output) authentication required;
+  typedef structure {
+      string report_name;
+      obj_ref report_ref;
+      obj_ref structure_obj_ref;
+  } ImportMSAOutput;
 
+  /* import_msa_file: import a MSA from FASTA*/
+  funcdef import_msa_file (ImportMSAParams params) returns (ImportMSAOutput result) authentication required;
+
+    typedef structure {
+        string file_path;
+    } MSAFilesOutput;
+
+    typedef structure {
+        string msa_name;
+        string workspace_name;
+    } MSAToFileParams;
+
+    funcdef msa_to_fasta_file(MSAToFileParams params)
+                returns(MSAFilesOutput files) authentication required;
+
+    funcdef msa_to_clustal_file(MSAToFileParams params)
+                returns(MSAFilesOutput files) authentication required;
+
+
+    typedef structure {
+        obj_ref input_ref;
+    } ExportParams;
+
+    typedef structure {
+        string shock_id;
+    } ExportOutput;
+
+    funcdef export_msa_as_fasta_file(ExportParams params)
+                returns (ExportOutput output) authentication required;
+
+    funcdef export_msa_as_clustal_file(ExportParams params)
+                returns (ExportOutput output) authentication required;
 };
--- lib/MSAUtils/MSAUtilsImpl.py
+++ lib/MSAUtils/MSAUtilsImpl.py
@@ -1,31 +1,156 @@
+# -*- coding: utf-8 -*-
 #BEGIN_HEADER
 import logging
 import os
 
 from installed_clients.KBaseReportClient import KBaseReport
 #END_HEADER
-#BEGIN_CLASS_HEADER
-#END_CLASS_HEADER
-#BEGIN_CONSTRUCTOR
+
+
+class MSAUtils:
+    '''
+    Module Name:
+    MSAUtils
+
+    Module Description:
+    A KBase module: MSAUtils
+    '''
+
+    ######## WARNING FOR GEVENT USERS ####### noqa
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    ######################################### noqa
+    VERSION = "0.0.1"
+    GIT_URL = "https://github.com/kbaseapps/MSAUtils.git"
+    GIT_COMMIT_HASH = "f33d1d9d21c06cbaf4df6d3408f5d25eac54ab0d"
+
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
         self.callback_url = os.environ['SDK_CALLBACK_URL']
         self.shared_folder = config['scratch']
         logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
                             level=logging.INFO)
-#END_CONSTRUCTOR
-#BEGIN run_MSAUtils
-        report = KBaseReport(self.callback_url)
-        report_info = report.create({'report': {'objects_created':[],
-                                                'text_message': params['parameter_1']},
-                                                'workspace_name': params['workspace_name']})
-        output = {
-            'report_name': report_info['name'],
-            'report_ref': report_info['ref'],
-        }
-#END run_MSAUtils
-#BEGIN_STATUS
+        #END_CONSTRUCTOR
+        pass
+
+
+    def import_msa_file(self, ctx, params):
+        """
+        import_msa_file: import a MSA from FASTA
+        :param params: instance of type "ImportMSAParams" -> structure:
+           parameter "input_shock_id" of String, parameter "input_file_path"
+           of String, parameter "input_staging_file_path" of String,
+           parameter "msa_name" of String, parameter "description" of String,
+           parameter "workspace_name" of String
+        :returns: instance of type "ImportMSAOutput" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of type "obj_ref"
+           (An X/Y/Z style reference @id ws), parameter "structure_obj_ref"
+           of type "obj_ref" (An X/Y/Z style reference @id ws)
+        """
+        # ctx is the context object
+        # return variables are: result
+        #BEGIN import_msa_file
+        #END import_msa_file
+
+        # At some point might do deeper type checking...
+        if not isinstance(result, dict):
+            raise ValueError('Method import_msa_file return value ' +
+                             'result is not type dict as required.')
+        # return the results
+        return [result]
+
+    def msa_to_fasta_file(self, ctx, params):
+        """
+        :param params: instance of type "MSAToFileParams" -> structure:
+           parameter "msa_name" of String, parameter "workspace_name" of
+           String
+        :returns: instance of type "MSAFilesOutput" -> structure: parameter
+           "file_path" of String
+        """
+        # ctx is the context object
+        # return variables are: files
+        #BEGIN msa_to_fasta_file
+        #END msa_to_fasta_file
+
+        # At some point might do deeper type checking...
+        if not isinstance(files, dict):
+            raise ValueError('Method msa_to_fasta_file return value ' +
+                             'files is not type dict as required.')
+        # return the results
+        return [files]
+
+    def msa_to_clustal_file(self, ctx, params):
+        """
+        :param params: instance of type "MSAToFileParams" -> structure:
+           parameter "msa_name" of String, parameter "workspace_name" of
+           String
+        :returns: instance of type "MSAFilesOutput" -> structure: parameter
+           "file_path" of String
+        """
+        # ctx is the context object
+        # return variables are: files
+        #BEGIN msa_to_clustal_file
+        #END msa_to_clustal_file
+
+        # At some point might do deeper type checking...
+        if not isinstance(files, dict):
+            raise ValueError('Method msa_to_clustal_file return value ' +
+                             'files is not type dict as required.')
+        # return the results
+        return [files]
+
+    def export_msa_as_fasta_file(self, ctx, params):
+        """
+        :param params: instance of type "ExportParams" -> structure:
+           parameter "input_ref" of type "obj_ref" (An X/Y/Z style reference
+           @id ws)
+        :returns: instance of type "ExportOutput" -> structure: parameter
+           "shock_id" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN export_msa_as_fasta_file
+        #END export_msa_as_fasta_file
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method export_msa_as_fasta_file return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+
+    def export_msa_as_clustal_file(self, ctx, params):
+        """
+        :param params: instance of type "ExportParams" -> structure:
+           parameter "input_ref" of type "obj_ref" (An X/Y/Z style reference
+           @id ws)
+        :returns: instance of type "ExportOutput" -> structure: parameter
+           "shock_id" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN export_msa_as_clustal_file
+        #END export_msa_as_clustal_file
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method export_msa_as_clustal_file return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+    def status(self, ctx):
+        #BEGIN_STATUS
         returnVal = {'state': "OK",
                      'message': "",
                      'version': self.VERSION,
                      'git_url': self.GIT_URL,
                      'git_commit_hash': self.GIT_COMMIT_HASH}
-#END_STATUS
+        #END_STATUS
+        return [returnVal]
